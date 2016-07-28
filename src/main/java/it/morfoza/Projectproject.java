4c256f15aae2d715a15dc8d5f800eb59d0886854
Wojt <ciechman.ws@gmail.com>
2016-07-28 22:29:38 +0200

    Project

---
+++ src/main/java/it/morfoza/Projectproject.java
@@ -0,0 +1,27 @@
+package it.morfoza;
+
+
+import spark.Spark;
+
+/**
+ * Created by Wojt on 2016-07-28.
+ */
+public class Projectproject {
+
+    public static void main(String[] args) {
+
+/*        String port = System.getenv("PORT");
+        if (port != null) {
+            int portInt = Integer.parseInt(port);
+            Spark.port(portInt);
+*/
+        Spark.staticFileLocation("/webfiles");
+
+        Spark.get("/hello", (request, response) -> {
+            return "<html> Hello <b> Buddy </b> <br/>" +
+                    "Please got to /project.html" +
+                    "</html>";
+        });
+
+    }
+}
