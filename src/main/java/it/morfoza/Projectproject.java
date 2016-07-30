7c8831c2ef9c901cce906a1b8a62c3a62c9ca656
Wojt <ciechman.ws@gmail.com>
2016-07-30 17:04:31 +0200

    new line at the end of Procfile, port set/corrected

--- src/main/java/it/morfoza/Projectproject.java
+++ src/main/java/it/morfoza/Projectproject.java
@@ -10,18 +10,19 @@ public class Projectproject {
 
     public static void main(String[] args) {
 
-      String port = System.getenv("PORT");
+        String port = System.getenv("PORT");
         if (port != null) {
             int portInt = Integer.parseInt(port);
             Spark.port(portInt);
 
-        Spark.staticFileLocation("/webfiles");
+            Spark.staticFileLocation("/webfiles");
 
-        Spark.get("/hello", (request, response) -> {
-            return "<html> Hello <b> Buddy </b> <br/>" +
-                    "Please got to /project.html" +
-                    "</html>";
-        });
+            Spark.get("/hello", (request, response) -> {
+                return "<html> Hello <b> Buddy </b> <br/>" +
+                        "Please got to /project.html" +
+                        "</html>";
+            });
 
+        }
     }
 }
